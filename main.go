c601682c0dd779d9d505a142043f9c14a599245b
dnieh <danielnieh@gmail.com>
2016-02-09 08:45:12 -0800

    Initial commit.

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const httpsPort = ":10443" // change to 443 as root on your actual server
+const httpPort = ":8080"
+
+func forceHTTPS() {
+	http.ListenAndServe(httpPort, http.RedirectHandler("https://127.0.0.1"+httpsPort, http.StatusFound))
+}
+
+func serveSomething(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("You've been served."))
+}
+
+func main() {
+	go forceHTTPS()
+	http.HandleFunc("/", serveSomething)
+	// generate cert.pem and key.pem by running src/crypto/tls/generate_cert.go
+	log.Fatal(http.ListenAndServeTLS(httpsPort, "cert.pem", "key.pem", nil))
+}
